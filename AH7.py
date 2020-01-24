d88c03d3cb56a703cc5a4a41938f13d8e8d41663
Alharm7 <55413454+Alharm7@users.noreply.github.com>
2020-01-24 12:38:06 -0800

    Add files via upload

---
+++ AH7.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+# - * - coding: utf-8 - * -
+def Style():
+	from V7xStyle import Animation
+	text = '''\033[0;32m\n     ___   _       _   _       ___   _____        ___  ___   _____\n    /   | | |     | | | |     /   | |  _  \\      /   |/   | |___  |\n   / /| | | |     | |_| |    / /| | | |_| |     / /|   /| |     / /\n  / / | | | |     |  _  |   / / | | |  _  /    / / |__/ | |    / /\n / /  | | | |___  | | | |  / /  | | | | \\ \\   / /       | |   / /\n/_/   |_| |_____| |_| |_| /_/   |_| |_|  \\_\\ /_/ark web  _|  /_/\033[0;34myoussef.sh\n                             \033[0;36m╔═════════════╗\n                             \033[0;36m║\033[0;37mAlharm v{1.0}\033[0;36m║\n                             \033[0;36m╚═════════════╝\n     \033[0;37m╔══════════════════════════════╗\033[0;37m╔══════════════════════════════╗\n     \033[0;37m║\033[0;37m        \033[0;32m[\033[0;33m1\033[0;32m] dark.media        \033[0;37m║\033[0;37m║\033[0;37m        \033[0;32m[\033[0;33m2\033[0;32m] earn.mony         \033[0;37m║\n     \033[0;37m╚══════════════════════════════╝\033[0;37m╚══════════════════════════════╝\n     \033[0;37m╔══════════════════════════════╗\033[0;37m╔══════════════════════════════╗\n     \033[0;37m║\033[0;37m        \033[0;32m[\033[0;33m3\033[0;32m] Chat.rooms        \033[0;37m║\033[0;37m║\033[0;37m      \033[0;32m[\033[0;33m4\033[0;32m] dark.web.games      \033[0;37m║\n     \033[0;37m╚══════════════════════════════╝\033[0;37m╚══════════════════════════════╝\n     \033[0;37m╔══════════════════════════════╗\033[0;37m╔══════════════════════════════╗\n     \033[0;37m║\033[0;37m          \033[0;32m[\033[0;33m5\033[0;32m] tools           \033[0;37m║\033[0;37m║\033[0;37m       \033[0;32m[\033[0;33m6\033[0;32m] cyber.hacker       \033[0;37m║\n     \033[0;37m╚══════════════════════════════╝\033[0;37m╚══════════════════════════════╝\n                               \033[0;36m╔═════════╗\n                               \033[0;36m║\033[0;37mex [A] it\033[0;36m║\n                               \033[0;36m╚═════════╝'''
+	Animation.SlowLine(text,time=0.0001)
+Style()
+def AH7():
+    Ah7 = input ('  {*}==}}  ')
+    if Ah7 == "1" :
+       print ('')
+       print ('http://zqktlwi4fecvo6ri.onion/wiki/index.php/Main_Page')
+       print ('')
+       print ('face book on dark web ...https://www.facebookcorewwwi.onion/.....')
+       print ('')
+    if Ah7 == "2" :
+       print ('')
+       print ('')
+       print ('http://encryptor3awk6px.onion/ – Encryptor Raas')
+       print ('')
+       print ('http://7kuslhhtlooylgu4.onion/ – Perl Script')
+       print ('')
+       print ('http://6dvj6v5imhny3anf.onion/ – Cyber ​​Guerrilla')
+       print ('')
+    if Ah7 == "3" :
+       print ('')
+       print ('')
+       print ('http://r2j4xiyckibnyd45.onion/ – BoyChat')
+       print ('')
+       print ('http://adzshort.com/HmooAD3f')
+       print ('')
+    if Ah7 == "4" :
+       print ('')
+       print ('http://bettorzztykidrx2.onion')
+       print ('')
+       print ('http://theches3nacocgsc.onion')
+       print ('')
+       print ('http://betcoinahk4j27yb.onion')
+       print ('')
+       print ('http://5p6dpc344vsbigv7.onion')
+       print ('')
+    if Ah7 == "5" :
+       print ('')
+       print ('http://exoduockgfq3ikf7.onion ')
+       print ('')
+       print ('http://atlas777hhh7mcs7.onion')
+       print ('')
+       print ('http://distropbucaekpgf.onion/')
+       print ('')
+    if Ah7 == "6" :
+       print ('')
+       print ('')
+       print ('http://edsec5zn26zqjwry.onion')
+       print ('')
+       print ('')
+    if Ah7 == "A" :
+          exit
+AH7()
